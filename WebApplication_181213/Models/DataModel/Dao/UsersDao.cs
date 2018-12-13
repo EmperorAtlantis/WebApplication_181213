f79791775a2bee50fbafedd4b850094bd6650af7
EmperorAtlantis <1968297406@qq.com>
2018-12-13 19:57:33 +0800

    181213 19.57

---
+++ WebApplication_181213/Models/DataModel/Dao/UsersDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_181213.Models.DataModel.Dao
+{
+    public class UsersDao
+    {
+    }
+}
---
+++ WebApplication_181213/Models/DataModel/Entity/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WebApplication_181213.Models.DataModel.Entity
+{
+    public class Users
+    {
+        private long Id { get; set; }
+
+        private string Name { get; set; }
+
+        private string Pwd { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
