7ff4b736f44648f6c818f9929d36cf74c22496bb
Your Name <you@example.com>
2018-12-13 16:57:57 +0800

    181213 16.57

--- WebApplication_181213/App_Start/BundleConfig.cs
+++ WebApplication_181213/App_Start/BundleConfig.cs
@@ -19,12 +19,12 @@ namespace WebApplication_181213
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/metro").Include(
+                      "~/Scripts/metro.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/metro-all.css",
+                      "~/Content/Site.css"));
         }
     }
 }
--- WebApplication_181213/Controllers/HomeController.cs
+++ WebApplication_181213/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace WebApplication_181213.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Welcome!";
+
+            return View();
+        }
     }
 }
